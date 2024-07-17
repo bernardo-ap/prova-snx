8b98b011e217e697d7e08203502d666c9ea62dfb
Bernardo de Andrade Peçanha <bernardo66026@gmail.com>
2024-07-16 21:02:47 -0300

    criando componente de post

---
+++ components/post/post.js
@@ -0,0 +1,8 @@
+class Post {
+    constructor(postQuery) {
+        this.postQuery = postQuery;
+    }
+
+}
+
+module.exports = new Post();
---
+++ components/post/query/post-query.js
@@ -0,0 +1,9 @@
+const connector = require('../../../database/connector');
+
+class PostQuery {
+
+    constructor() {
+    }
+}
+
+module.exports = new PostQuery();
